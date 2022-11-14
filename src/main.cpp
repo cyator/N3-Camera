df53ed2d8cf4e3d932ce29ce5a76a6fe494f948d
cyator <christianyator7@gmail.com>
2022-11-14 22:01:01 +0300

    feat: mjpeg and websockets

---
+++ src/main.cpp
@@ -0,0 +1,233 @@
+#include <Arduino.h>
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <ArduinoWebsockets.h>
+
+#define CAMERA_MODEL_AI_THINKER
+
+#include "camera_pins.h"
+
+const char *ssid = "S7 edge";
+const char *password = "almg76061";
+
+const char *websockets_server_host = "192.168.43.104";
+const uint16_t websockets_server_port = 3000;
+
+using namespace websockets;
+WebsocketsClient client;
+
+typedef struct
+{
+  size_t size;  // number of values used for filtering
+  size_t index; // current value index
+  size_t count; // value count
+  int sum;
+  int *values; // array to be filled with values
+} ra_filter_t;
+
+static ra_filter_t ra_filter;
+
+static ra_filter_t *ra_filter_init(ra_filter_t *filter, size_t sample_size)
+{
+  memset(filter, 0, sizeof(ra_filter_t));
+
+  filter->values = (int *)malloc(sample_size * sizeof(int));
+  if (!filter->values)
+  {
+    return NULL;
+  }
+  memset(filter->values, 0, sample_size * sizeof(int));
+
+  filter->size = sample_size;
+  return filter;
+}
+
+static int ra_filter_run(ra_filter_t *filter, int value)
+{
+  if (!filter->values)
+  {
+    return value;
+  }
+  filter->sum -= filter->values[filter->index];
+  filter->values[filter->index] = value;
+  filter->sum += filter->values[filter->index];
+  filter->index++;
+  filter->index = filter->index % filter->size;
+  if (filter->count < filter->size)
+  {
+    filter->count++;
+  }
+  return filter->sum / filter->count;
+}
+
+static esp_err_t handleStream()
+{
+  camera_fb_t *fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len = 0;
+  uint8_t *_jpg_buf = NULL;
+
+  static int64_t last_frame = 0;
+  if (!last_frame)
+  {
+    last_frame = esp_timer_get_time();
+  }
+
+  while (true)
+  {
+    fb = esp_camera_fb_get();
+    if (!fb)
+    {
+      Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    }
+    else
+    {
+
+      if (fb->format != PIXFORMAT_JPEG)
+      {
+        bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+        esp_camera_fb_return(fb);
+        fb = NULL;
+        if (!jpeg_converted)
+        {
+          Serial.println("JPEG compression failed");
+          res = ESP_FAIL;
+        }
+      }
+      else
+      {
+        _jpg_buf_len = fb->len;
+        _jpg_buf = fb->buf;
+      }
+    }
+    if (res == ESP_OK)
+    {
+      client.sendBinary((const char *)_jpg_buf, _jpg_buf_len);
+    }
+
+    if (fb)
+    {
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      _jpg_buf = NULL;
+    }
+    else if (_jpg_buf)
+    {
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    if (res != ESP_OK)
+    {
+      break;
+    }
+    int64_t fr_end = esp_timer_get_time();
+
+    int64_t frame_time = fr_end - last_frame;
+    last_frame = fr_end;
+    frame_time /= 1000;
+    uint32_t avg_frame_time = ra_filter_run(&ra_filter, frame_time);
+    Serial.printf("MJPG: %uB %ums (%.1ffps), AVG: %ums (%.1ffps)\n",
+                  (uint32_t)(_jpg_buf_len),
+                  (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time,
+                  avg_frame_time, 1000.0 / avg_frame_time);
+  }
+  last_frame = 0;
+  return res;
+}
+
+void startStream()
+{
+  ra_filter_init(&ra_filter, 20);
+  handleStream();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  // init with high specs to pre-allocate larger buffers
+  if (psramFound())
+  {
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  }
+  else
+  {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  sensor_t *s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID)
+  {
+    s->set_vflip(s, 1);       // flip it back
+    s->set_brightness(s, 1);  // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  while (!client.connect(websockets_server_host, websockets_server_port, "/"))
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("Websocket Connected!");
+
+  startStream();
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  delay(10000);
+}
